1765615c4e58be677b8b99c3650e4cf2e97e0a07
Ashish Kumar <54205379+ashishbhs@users.noreply.github.com>
2024-09-23 23:14:26 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def extract_text_from_pdf(pdf_file):
 
 def vector_embedding(pdf_file):
     if "vectors" not in st.session_state:
+        st.session_state.vectors = None  # Initialize it to None or any default value
         st.session_state.embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
         # Extract text from the uploaded PDF file
@@ -71,17 +72,20 @@ if st.button("Documents Embedding") and uploaded_file is not None:
     st.write("Vector Store DB Is Ready")
 
 if prompt1:
-    document_chain = create_stuff_documents_chain(llm, prompt)
-    retriever = st.session_state.vectors.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    start = time.process_time()
-    response = retrieval_chain.invoke({'input': prompt1})
-    print("Response time :", time.process_time() - start)
-    st.write(response['answer'])
+    if "vectors" in st.session_state:  # Check if vectors have been initialized
+        document_chain = create_stuff_documents_chain(llm, prompt)
+        retriever = st.session_state.vectors.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        start = time.process_time()
+        response = retrieval_chain.invoke({'input': prompt1})
+        print("Response time :", time.process_time() - start)
+        st.write(response['answer'])
 
-    # With a Streamlit expander
-    with st.expander("Document Similarity Search"):
-        # Find the relevant chunks
-        for i, doc in enumerate(response["context"]):
-            st.write(doc.page_content)
-            st.write("--------------------------------")
+        # With a Streamlit expander
+        with st.expander("Document Similarity Search"):
+            # Find the relevant chunks
+            for i, doc in enumerate(response["context"]):
+                st.write(doc.page_content)
+                st.write("--------------------------------")
+    else:
+        st.warning("Please upload a document and generate the vector store first.")
